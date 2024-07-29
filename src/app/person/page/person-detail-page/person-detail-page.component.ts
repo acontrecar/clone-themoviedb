4f83c39cb345d988aa68a07b796c9d087e562d94
Antonio Contreras <acontrecar@gmail.com>
2024-07-29 18:05:53 +0200

    Person list with pagination

---
+++ src/app/person/page/person-detail-page/person-detail-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-person-detail-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './person-detail-page.component.html',
+  styleUrl: './person-detail-page.component.scss'
+})
+export class PersonDetailPageComponent {
+
+}
